1663d7342e72b16822fe1f3e4aabe6449c74e4af
JanikSB <janiksb.telekom@gmail.com>
2023-03-30 11:00:01 +0000

    Meine Buchung fertig

--- webapp/controller/MeineBuchung.controller.js
+++ webapp/controller/MeineBuchung.controller.js
@@ -8,14 +8,18 @@ sap.ui.define([
 
     "sap/ui/commons/Label",
     "sap/ui/commons/TextView",
-    "sap/ui/table/Column"
-], function (Controller, Filter, FilterOperator, MessageToast, DateFormat, Label, TextView, Column) {
+    "sap/ui/table/Column",
+
+
+    'sap/m/ColumnListItem',
+    'sap/m/Text',
+    'sap/m/ObjectIdentifier'
+], function (Controller, Filter, FilterOperator, MessageToast, DateFormat, Label, TextView, Column, ColumnListItem, Text, ObjectIdentifier) {
     'use strict';
 
 
     var localName;
     var that;
-    var oModel;
 
 
 
@@ -33,73 +37,123 @@ sap.ui.define([
               }
 
             //odata model auslesen
-            this.readBookings();
+            // this.readBookings();
+            this.bindItems();
         },
+
+        bindItems(){
+            var oTable = this.getView().byId('myTable');
+            oTable.bindItems({
+                path: 'Model>/BOOKINGS',
+                filters: [
+                    new Filter('bookername', FilterOperator.EQ, localName)
+                ],
+                
+                template: new ColumnListItem({
+                    cells: [
+                        new ObjectIdentifier({
+                            title: '{Model>raumnummer}'
+                        }),
+                        
+                        new Text({
+                            text:{parts: [{path: 'Model>datetimefrom'}],
+                                formatter: that.dateFormatter.bind(this)
+                            }
+                        }),
+
+                        new Text({
+                            text:{parts: [{path: 'Model>datetimeto'}],
+                                formatter: that.dateFormatter.bind(this)
+                            }
+                        }),
+
+                        new Text({
+                            text:
+                            {parts: ['Model>datetimefrom', 'Model>datetimeto'],
+                            formatter: (datetimefrom, datetimeto) => {
+                                return that.statusFormatter(datetimefrom, datetimeto);
+                            }}
+                        }),
+
+                        new Text({
+                            text: '{Model>bookingid}'
+                        })
+                    ]
+                })
+            })
+        },
+
+
+
+
+
 
 
         //tabelle mit eigenen buchungen fuellen
-        readBookings(){
 
-            var oTable = this.getView().byId('myTable');
+        // readBookings(){
 
-            oTable.bindRows({
-                path: "Model>/BOOKINGS",
-                //set the filter for the bookername property
-                filters: [
-                    new Filter("bookername", FilterOperator.EQ, localName)
-                ],
-                parameters: {
-                    //set the count mode to inline to get the number of records for paging
-                    countMode: 'Inline'
-                }
-            });
+        //     var oTable = this.getView().byId('myTable');
 
-            oTable.addColumn(new Column({
-                label: new Label({text: "Raumnummer"}),
-                template: new TextView({
-                    text: '{Model>raumnummer}'})
-            }));
+        //     oTable.bindRows({
+        //         path: "Model>/BOOKINGS",
+        //         //set the filter for the bookername property
+        //         filters: [
+        //             new Filter("bookername", FilterOperator.EQ, localName)
+        //         ],
+        //         parameters: {
+        //             //set the count mode to inline to get the number of records for paging
+        //             countMode: 'Inline'
+        //         }
+        //     });
+
+        //     oTable.addColumn(new Column({
+        //         label: new Label({text: "Raumnummer"}),
+        //         template: new TextView({
+        //             text: '{Model>raumnummer}'})
+        //     }));
             
-            //add a column for the 'datetimefrom' property
-            oTable.addColumn(new Column({
-                label: new Label({text: "Von"}),
-                template: new TextView({
-                    text: 
-                    {parts: [{path: 'Model>datetimefrom'}],
-                    formatter: that.dateFormatter.bind(this)
-                }})
-            }));
+        //     //add a column for the 'datetimefrom' property
+        //     oTable.addColumn(new Column({
+        //         label: new Label({text: "Von"}),
+        //         template: new TextView({
+        //             text: 
+        //             {parts: [{path: 'Model>datetimefrom'}],
+        //             formatter: that.dateFormatter.bind(this)
+        //         }})
+        //     }));
             
-            //add a column for the 'datetimeto' property
-            oTable.addColumn(new Column({
-                label: new Label({text: "Bis"}),
-                template: new TextView({
-                    text: 
-                    {parts: [{path: 'Model>datetimeto'}],
-                    formatter: that.dateFormatter.bind(this)
-                }})
-            }));
+        //     //add a column for the 'datetimeto' property
+        //     oTable.addColumn(new Column({
+        //         label: new Label({text: "Bis"}),
+        //         template: new TextView({
+        //             text: 
+        //             {parts: [{path: 'Model>datetimeto'}],
+        //             formatter: that.dateFormatter.bind(this)
+        //         }})
+        //     }));
 
-            oTable.addColumn(new Column({
-                label: new Label({text: 'Status'}),
-                template: new TextView({
-                    text:
-                    {parts: ['Model>datetimefrom', 'Model>datetimeto'],
-                    formatter: (datetimefrom, datetimeto) => {
-                        return that.statusFormatter(datetimefrom, datetimeto);
-                    }
-                }})
-            }))
+        //     oTable.addColumn(new Column({
+        //         label: new Label({text: 'Status'}),
+        //         template: new TextView({
+        //             text:
+        //             {parts: ['Model>datetimefrom', 'Model>datetimeto'],
+        //             formatter: (datetimefrom, datetimeto) => {
+        //                 return that.statusFormatter(datetimefrom, datetimeto);
+        //             }
+        //         }})
+        //     }))
 
-            oTable.addColumn(new Column({
-                label: new Label({text: 'Buchungsnummer'}),
-                template: new TextView({
-                    text: '{Model>bookingid}'}),
-                visible: false
-            }))
-        },
+        //     oTable.addColumn(new Column({
+        //         label: new Label({text: 'Buchungsnummer'}),
+        //         template: new TextView({
+        //             text: '{Model>bookingid}'}),
+        //         visible: false
+        //     }))
+        // },
 
         //datum zu lesbarem string formattieren
+        
         dateFormatter(date){
             const format = DateFormat.getDateTimeInstance({pattern: "dd.MM.yyyy HH:mm"});
             var strDate = format.format(new Date(date));
@@ -132,7 +186,7 @@ sap.ui.define([
                 aSelectedItems = oSource.getSelectedItems(),
                 sTitle = oSource.getTitle(),
                 oTable = this.byId("myTable"),
-                oBinding = oTable.getBinding("rows"),
+                oBinding = oTable.getBinding("items"),
                 aFilters = [];
 
             if (aSelectedItems.length > 0) {
@@ -142,10 +196,6 @@ sap.ui.define([
                         aFilters.push(new sap.ui.model.Filter("raumnummer", "EQ", sKey));
                     } 
 
-                    // STATUS AENDERN -----------------------------------------
-                    else if (sTitle === "Status") {
-                        aFilters.push(new sap.ui.model.Filter("status", "EQ", sKey));
-                    }
                 }
 
                 
@@ -162,21 +212,22 @@ sap.ui.define([
             var model = that.getOwnerComponent().getModel('Model');          
 
             var oTable = this.byId('myTable');
-            var selectedBookingsIndices = oTable.getSelectedIndices();
+            var test = oTable.getBinding('items').getContexts();
+
+            var aSelectedItems = oTable.getSelectedItems();
 
             
             //check ob eine Buchung ausgewaehlt wurde
-            if(selectedBookingsIndices.length > 0){
+            if(aSelectedItems.length > 0){
 
                 //durch ausgewaehlte buchungen loopen und loeschen
-                for(var i= 0; i < selectedBookingsIndices.length; i++){
-                    let oContext = oTable.getContextByIndex(selectedBookingsIndices[i])
-                    let object = oContext.getObject();
-
-                    let booking = object.bookingid;
+                for(var i= 0; i < aSelectedItems.length; i++){
+                    let oSelectedItem = aSelectedItems[i];
+                    let aCells = oSelectedItem.getCells();
+                    let sBookingId = aCells[4].getText();
 
                     //booking aus model entfernen
-                    model.remove(`/BOOKINGS(${booking})`, {
+                    model.remove(`/BOOKINGS(${sBookingId})`, {
                         method: 'DELETE',
                         success: (data) => {
                             console.log('erfolreich aus model geloescht');
